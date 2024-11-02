f00ef13ad987b31aa8f358e853cd55685e3770ec
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-02 21:15:29 +0200

    Update tests.mk

--- tests.mk
+++ tests.mk
@@ -1,75 +1,47 @@
 #!/usr/bin/make -f
 
-########################################
-### Testing
-
 BINDIR ?= $(GOPATH)/bin
+PACKAGES ?= ./...
+TEST_FLAGS ?= -p 1
+DOCKER_TEST_IMAGE := tester
 
-## required to be run first by most tests
+.PHONY: test test_race test_deadlock test_release test100 \
+        test_cover test_apps test_abci_apps test_abci_cli \
+        test_integrations vagrant_test
+
 build_docker_test_image:
-	docker build -t tester -f ./test/docker/Dockerfile .
-.PHONY: build_docker_test_image
+	docker build -t $(DOCKER_TEST_IMAGE) -f ./test/docker/Dockerfile .
 
-### coverage, app, persistence, and libs tests
 test_cover:
-	# run the go unit tests with coverage
 	bash test/test_cover.sh
-.PHONY: test_cover
 
 test_apps:
-	# run the app tests using bash
-	# requires `abci-cli` and `cometbft` binaries installed
 	bash test/app/test.sh
-.PHONY: test_apps
 
 test_abci_apps:
 	bash abci/tests/test_app/test.sh
-.PHONY: test_abci_apps
 
 test_abci_cli:
-	# test the cli against the examples in the tutorial at:
-	# ./docs/abci-cli.md
-	# if test fails, update the docs ^
-	@ bash abci/tests/test_cli/test.sh
-.PHONY: test_abci_cli
+	bash abci/tests/test_cli/test.sh
 
-test_integrations:
-	make build_docker_test_image
-	make tools
-	make install
-	make install_abci
-	make test_cover
-	make test_apps
-	make test_abci_apps
-	make test_abci_cli
-	make test_libs
-.PHONY: test_integrations
+test_integrations: build_docker_test_image tools install install_abci \
+                  test_cover test_apps test_abci_apps test_abci_cli test_libs
 
 test_release:
-	@go test -tags release $(PACKAGES)
-.PHONY: test_release
+	go test -tags release $(PACKAGES)
 
 test100:
-	@for i in {1..100}; do make test; done
-.PHONY: test100
+	for i in {1..100}; do $(MAKE) test; done
 
 vagrant_test:
 	vagrant up
 	vagrant ssh -c 'make test_integrations'
-.PHONY: vagrant_test
 
-### go tests
 test:
-	@echo "--> Running go test"
-	@go test -p 1 $(PACKAGES) -tags deadlock
-.PHONY: test
+	go test $(TEST_FLAGS) $(PACKAGES) -tags deadlock
 
 test_race:
-	@echo "--> Running go test --race"
-	@go test -p 1 -v -race $(PACKAGES)
-.PHONY: test_race
+	go test $(TEST_FLAGS) -v -race $(PACKAGES)
 
 test_deadlock:
-	@echo "--> Running go test --deadlock"
-	@go test -p 1 -v  $(PACKAGES) -tags deadlock 
-.PHONY: test_race
+	go test $(TEST_FLAGS) -v $(PACKAGES) -tags deadlock
