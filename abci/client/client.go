76488e8af36103aaa9789761fc4640778b283489
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-03 23:32:42 +0200

    Update client.go

--- abci/client/client.go
+++ abci/client/client.go
@@ -14,19 +14,12 @@ const (
 	echoRetryIntervalSeconds = 1
 )
 
-//go:generate ../../scripts/mockery_generate.sh Client
-
-// Client defines an interface for an ABCI client.
-// All `Async` methods return a `ReqRes` object.
-// All `Sync` methods return the appropriate protobuf ResponseXxx struct and an error.
-// Note these are client errors, eg. ABCI socket connectivity issues.
-// Application-related errors are reflected in response via ABCI error codes and logs.
 type Client interface {
 	service.Service
-
 	SetResponseCallback(Callback)
 	Error() error
 
+	// Async methods
 	FlushAsync() *ReqRes
 	EchoAsync(msg string) *ReqRes
 	InfoAsync(types.RequestInfo) *ReqRes
@@ -44,6 +37,7 @@ type Client interface {
 	ApplySnapshotChunkAsync(types.RequestApplySnapshotChunk) *ReqRes
 	ProcessProposalAsync(types.RequestProcessProposal) *ReqRes
 
+	// Sync methods
 	FlushSync() error
 	EchoSync(msg string) (*types.ResponseEcho, error)
 	InfoSync(types.RequestInfo) (*types.ResponseInfo, error)
@@ -62,69 +56,50 @@ type Client interface {
 	ProcessProposalSync(types.RequestProcessProposal) (*types.ResponseProcessProposal, error)
 }
 
-//----------------------------------------
+type Callback func(*types.Request, *types.Response)
 
-// NewClient returns a new ABCI client of the specified transport type.
-// It returns an error if the transport is not "socket" or "grpc"
-func NewClient(addr, transport string, mustConnect bool) (client Client, err error) {
+func NewClient(addr, transport string, mustConnect bool) (Client, error) {
 	switch transport {
 	case "socket":
-		client = NewSocketClient(addr, mustConnect)
+		return NewSocketClient(addr, mustConnect), nil
 	case "grpc":
-		client = NewGRPCClient(addr, mustConnect)
+		return NewGRPCClient(addr, mustConnect), nil
 	default:
-		err = fmt.Errorf("unknown abci transport %s", transport)
+		return nil, fmt.Errorf("unsupported transport: %s", transport)
 	}
-	return
 }
-
-type Callback func(*types.Request, *types.Response)
 
 type ReqRes struct {
 	*types.Request
 	*sync.WaitGroup
-	*types.Response // Not set atomically, so be sure to use WaitGroup.
+	*types.Response
 
-	mtx cmtsync.Mutex
-
-	// callbackInvoked as a variable to track if the callback was already
-	// invoked during the regular execution of the request. This variable
-	// allows clients to set the callback simultaneously without potentially
-	// invoking the callback twice by accident, once when 'SetCallback' is
-	// called and once during the normal request.
+	mtx             cmtsync.Mutex
+	cb              func(*types.Response)
 	callbackInvoked bool
-	cb              func(*types.Response) // A single callback that may be set.
 }
 
 func NewReqRes(req *types.Request) *ReqRes {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	
 	return &ReqRes{
 		Request:   req,
-		WaitGroup: waitGroup1(),
-		Response:  nil,
-
-		callbackInvoked: false,
-		cb:              nil,
+		WaitGroup: wg,
 	}
 }
 
-// Sets sets the callback. If reqRes is already done, it will call the cb
-// immediately. Note, reqRes.cb should not change if reqRes.done and only one
-// callback is supported.
 func (r *ReqRes) SetCallback(cb func(res *types.Response)) {
 	r.mtx.Lock()
+	defer r.mtx.Unlock()
 
 	if r.callbackInvoked {
-		r.mtx.Unlock()
 		cb(r.Response)
 		return
 	}
-
 	r.cb = cb
-	r.mtx.Unlock()
 }
 
-// InvokeCallback invokes a thread-safe execution of the configured callback
-// if non-nil.
 func (r *ReqRes) InvokeCallback() {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
@@ -135,20 +110,8 @@ func (r *ReqRes) InvokeCallback() {
 	r.callbackInvoked = true
 }
 
-// GetCallback returns the configured callback of the ReqRes object which may be
-// nil. Note, it is not safe to concurrently call this in cases where it is
-// marked done and SetCallback is called before calling GetCallback as that
-// will invoke the callback twice and create a potential race condition.
-//
-// ref: https://github.com/tendermint/tendermint/issues/5439
 func (r *ReqRes) GetCallback() func(*types.Response) {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
 	return r.cb
-}
-
-func waitGroup1() (wg *sync.WaitGroup) {
-	wg = &sync.WaitGroup{}
-	wg.Add(1)
-	return
 }
