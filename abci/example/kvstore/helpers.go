7a21600322ff13667ab14077f7cca4669dc0bdc6
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-03 23:39:56 +0200

    Update helpers.go

--- abci/example/kvstore/helpers.go
+++ abci/example/kvstore/helpers.go
@@ -1,36 +1,96 @@
 package kvstore
 
 import (
+	"errors"
+
 	"github.com/cometbft/cometbft/abci/types"
 	cmtrand "github.com/cometbft/cometbft/libs/rand"
 )
 
-// RandVal creates one random validator, with a key derived
-// from the input value
-func RandVal(i int) types.ValidatorUpdate {
-	pubkey := cmtrand.Bytes(32)
-	power := cmtrand.Uint16() + 1
-	v := types.UpdateValidator(pubkey, int64(power), "")
-	return v
+const (
+	defaultPubKeyLength = 32
+	minValidatorCount  = 1
+	maxValidatorCount  = 100
+)
+
+var (
+	ErrInvalidValidatorCount = errors.New("validator count must be between 1 and 100")
+	ErrNilApplication       = errors.New("application cannot be nil")
+)
+
+// ValidatorGenerator handles creation of validator updates
+type ValidatorGenerator struct {
+	pubKeyLength int
 }
 
-// RandVals returns a list of cnt validators for initializing
-// the application. Note that the keys are deterministically
-// derived from the index in the array, while the power is
-// random (Change this if not desired)
-func RandVals(cnt int) []types.ValidatorUpdate {
-	res := make([]types.ValidatorUpdate, cnt)
-	for i := 0; i < cnt; i++ {
-		res[i] = RandVal(i)
+// NewValidatorGenerator creates a new validator generator with custom settings
+func NewValidatorGenerator(pubKeyLength int) *ValidatorGenerator {
+	if pubKeyLength <= 0 {
+		pubKeyLength = defaultPubKeyLength
 	}
-	return res
+	
+	return &ValidatorGenerator{
+		pubKeyLength: pubKeyLength,
+	}
 }
 
-// InitKVStore initializes the kvstore app with some data,
-// which allows tests to pass and is fine as long as you
-// don't make any tx that modify the validator state
-func InitKVStore(app *PersistentKVStoreApplication) {
+// GenerateValidator creates a single random validator with deterministic public key
+// derived from the input index and random power value
+func (vg *ValidatorGenerator) GenerateValidator(index int) types.ValidatorUpdate {
+	pubkey := cmtrand.Bytes(vg.pubKeyLength)
+	power := int64(cmtrand.Uint16() + 1) // Ensure non-zero power
+	
+	return types.UpdateValidator(pubkey, power, "")
+}
+
+// GenerateValidators creates a specified number of random validators
+func (vg *ValidatorGenerator) GenerateValidators(count int) ([]types.ValidatorUpdate, error) {
+	if count < minValidatorCount || count > maxValidatorCount {
+		return nil, ErrInvalidValidatorCount
+	}
+
+	validators := make([]types.ValidatorUpdate, count)
+	for i := 0; i < count; i++ {
+		validators[i] = vg.GenerateValidator(i)
+	}
+	
+	return validators, nil
+}
+
+// InitializeApp initializes a KVStore application with default settings
+func InitializeApp(app *PersistentKVStoreApplication) error {
+	if app == nil {
+		return ErrNilApplication
+	}
+
+	generator := NewValidatorGenerator(defaultPubKeyLength)
+	validators, err := generator.GenerateValidators(minValidatorCount)
+	if err != nil {
+		return err
+	}
+
 	app.InitChain(types.RequestInitChain{
-		Validators: RandVals(1),
+		Validators: validators,
 	})
+
+	return nil
+}
+
+// InitializeAppWithValidators initializes a KVStore application with a specific number of validators
+func InitializeAppWithValidators(app *PersistentKVStoreApplication, validatorCount int) error {
+	if app == nil {
+		return ErrNilApplication
+	}
+
+	generator := NewValidatorGenerator(defaultPubKeyLength)
+	validators, err := generator.GenerateValidators(validatorCount)
+	if err != nil {
+		return err
+	}
+
+	app.InitChain(types.RequestInitChain{
+		Validators: validators,
+	})
+
+	return nil
 }
