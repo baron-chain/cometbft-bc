c6615ea4289e540948901f432f9f22464143ccff
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-04 23:08:22 +0200

    Update types.go

--- abci/types/types.go
+++ abci/types/types.go
@@ -3,57 +3,12 @@ package types
 import (
 	"bytes"
 	"encoding/json"
-
 	"github.com/cosmos/gogoproto/jsonpb"
 )
 
-const (
-	CodeTypeOK uint32 = 0
-)
+const CodeTypeOK uint32 = 0
 
-// IsOK returns true if Code is OK.
-func (r ResponseCheckTx) IsOK() bool {
-	return r.Code == CodeTypeOK
-}
-
-// IsErr returns true if Code is something other than OK.
-func (r ResponseCheckTx) IsErr() bool {
-	return r.Code != CodeTypeOK
-}
-
-// IsOK returns true if Code is OK.
-func (r ResponseDeliverTx) IsOK() bool {
-	return r.Code == CodeTypeOK
-}
-
-// IsErr returns true if Code is something other than OK.
-func (r ResponseDeliverTx) IsErr() bool {
-	return r.Code != CodeTypeOK
-}
-
-// IsOK returns true if Code is OK.
-func (r ResponseQuery) IsOK() bool {
-	return r.Code == CodeTypeOK
-}
-
-// IsErr returns true if Code is something other than OK.
-func (r ResponseQuery) IsErr() bool {
-	return r.Code != CodeTypeOK
-}
-
-// IsAccepted returns true if Code is ACCEPT
-func (r ResponseProcessProposal) IsAccepted() bool {
-	return r.Status == ResponseProcessProposal_ACCEPT
-}
-
-// IsStatusUnknown returns true if Code is UNKNOWN
-func (r ResponseProcessProposal) IsStatusUnknown() bool {
-	return r.Status == ResponseProcessProposal_UNKNOWN
-}
-
-//---------------------------------------------------------------------------
-// override JSON marshaling so we emit defaults (ie. disable omitempty)
-
+// JSON encoding configuration
 var (
 	jsonpbMarshaller = jsonpb.Marshaler{
 		EnumsAsInts:  true,
@@ -62,69 +17,61 @@ var (
 	jsonpbUnmarshaller = jsonpb.Unmarshaler{}
 )
 
-func (r *ResponseCheckTx) MarshalJSON() ([]byte, error) {
-	s, err := jsonpbMarshaller.MarshalToString(r)
-	return []byte(s), err
+// Response status checks for different response types
+type Response interface {
+	IsOK() bool
+	IsErr() bool
 }
 
-func (r *ResponseCheckTx) UnmarshalJSON(b []byte) error {
-	reader := bytes.NewBuffer(b)
-	return jsonpbUnmarshaller.Unmarshal(reader, r)
-}
+func (r ResponseCheckTx) IsOK() bool        { return r.Code == CodeTypeOK }
+func (r ResponseCheckTx) IsErr() bool       { return r.Code != CodeTypeOK }
+func (r ResponseDeliverTx) IsOK() bool      { return r.Code == CodeTypeOK }
+func (r ResponseDeliverTx) IsErr() bool     { return r.Code != CodeTypeOK }
+func (r ResponseQuery) IsOK() bool          { return r.Code == CodeTypeOK }
+func (r ResponseQuery) IsErr() bool         { return r.Code != CodeTypeOK }
 
-func (r *ResponseDeliverTx) MarshalJSON() ([]byte, error) {
-	s, err := jsonpbMarshaller.MarshalToString(r)
-	return []byte(s), err
-}
+// ProcessProposal specific status checks
+func (r ResponseProcessProposal) IsAccepted() bool      { return r.Status == ResponseProcessProposal_ACCEPT }
+func (r ResponseProcessProposal) IsStatusUnknown() bool { return r.Status == ResponseProcessProposal_UNKNOWN }
 
-func (r *ResponseDeliverTx) UnmarshalJSON(b []byte) error {
-	reader := bytes.NewBuffer(b)
-	return jsonpbUnmarshaller.Unmarshal(reader, r)
-}
-
-func (r *ResponseQuery) MarshalJSON() ([]byte, error) {
-	s, err := jsonpbMarshaller.MarshalToString(r)
-	return []byte(s), err
-}
-
-func (r *ResponseQuery) UnmarshalJSON(b []byte) error {
-	reader := bytes.NewBuffer(b)
-	return jsonpbUnmarshaller.Unmarshal(reader, r)
-}
-
-func (r *ResponseCommit) MarshalJSON() ([]byte, error) {
-	s, err := jsonpbMarshaller.MarshalToString(r)
-	return []byte(s), err
-}
-
-func (r *ResponseCommit) UnmarshalJSON(b []byte) error {
-	reader := bytes.NewBuffer(b)
-	return jsonpbUnmarshaller.Unmarshal(reader, r)
-}
-
-func (r *EventAttribute) MarshalJSON() ([]byte, error) {
-	s, err := jsonpbMarshaller.MarshalToString(r)
-	return []byte(s), err
-}
-
-func (r *EventAttribute) UnmarshalJSON(b []byte) error {
-	reader := bytes.NewBuffer(b)
-	return jsonpbUnmarshaller.Unmarshal(reader, r)
-}
-
-// Some compile time assertions to ensure we don't
-// have accidental runtime surprises later on.
-
-// jsonEncodingRoundTripper ensures that asserted
-// interfaces implement both MarshalJSON and UnmarshalJSON
+// jsonRoundTripper ensures types implement both json.Marshaler and json.Unmarshaler
 type jsonRoundTripper interface {
 	json.Marshaler
 	json.Unmarshaler
 }
 
-var _ jsonRoundTripper = (*ResponseCommit)(nil)
-var _ jsonRoundTripper = (*ResponseQuery)(nil)
-var _ jsonRoundTripper = (*ResponseDeliverTx)(nil)
-var _ jsonRoundTripper = (*ResponseCheckTx)(nil)
+// marshalJSON is a helper function to implement common JSON marshaling logic
+func marshalJSON(r interface{}) ([]byte, error) {
+	s, err := jsonpbMarshaller.MarshalToString(r)
+	return []byte(s), err
+}
 
-var _ jsonRoundTripper = (*EventAttribute)(nil)
+// unmarshalJSON is a helper function to implement common JSON unmarshaling logic
+func unmarshalJSON(b []byte, r interface{}) error {
+	return jsonpbUnmarshaller.Unmarshal(bytes.NewBuffer(b), r)
+}
+
+// MarshalJSON/UnmarshalJSON implementations
+func (r *ResponseCheckTx) MarshalJSON() ([]byte, error)    { return marshalJSON(r) }
+func (r *ResponseCheckTx) UnmarshalJSON(b []byte) error    { return unmarshalJSON(b, r) }
+func (r *ResponseDeliverTx) MarshalJSON() ([]byte, error)  { return marshalJSON(r) }
+func (r *ResponseDeliverTx) UnmarshalJSON(b []byte) error  { return unmarshalJSON(b, r) }
+func (r *ResponseQuery) MarshalJSON() ([]byte, error)      { return marshalJSON(r) }
+func (r *ResponseQuery) UnmarshalJSON(b []byte) error      { return unmarshalJSON(b, r) }
+func (r *ResponseCommit) MarshalJSON() ([]byte, error)     { return marshalJSON(r) }
+func (r *ResponseCommit) UnmarshalJSON(b []byte) error     { return unmarshalJSON(b, r) }
+func (r *EventAttribute) MarshalJSON() ([]byte, error)     { return marshalJSON(r) }
+func (r *EventAttribute) UnmarshalJSON(b []byte) error     { return unmarshalJSON(b, r) }
+
+// Compile-time type assertions
+var (
+	_ Response = (*ResponseCheckTx)(nil)
+	_ Response = (*ResponseDeliverTx)(nil)
+	_ Response = (*ResponseQuery)(nil)
+	
+	_ jsonRoundTripper = (*ResponseCommit)(nil)
+	_ jsonRoundTripper = (*ResponseQuery)(nil)
+	_ jsonRoundTripper = (*ResponseDeliverTx)(nil)
+	_ jsonRoundTripper = (*ResponseCheckTx)(nil)
+	_ jsonRoundTripper = (*EventAttribute)(nil)
+)
