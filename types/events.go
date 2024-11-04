88955ac1ae935d909d4b59f4b9274baad1af68b4
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-04 23:23:26 +0200

    Update events.go

--- types/events.go
+++ types/events.go
@@ -3,10 +3,10 @@ package types
 import (
 	"fmt"
 
-	abci "github.com/cometbft/cometbft/abci/types"
-	cmtjson "github.com/cometbft/cometbft/libs/json"
-	cmtpubsub "github.com/cometbft/cometbft/libs/pubsub"
-	cmtquery "github.com/cometbft/cometbft/libs/pubsub/query"
+	abci "github.com/baron-chain/cometbft-bc/abci/types"
+	cmtjson "github.com/baron-chain/cometbft-bc/libs/json"
+	cmtpubsub "github.com/baron-chain/cometbft-bc/libs/pubsub"
+	cmtquery "github.com/baron-chain/cometbft-bc/libs/pubsub/query"
 )
 
 // Reserved event types (alphabetically sorted).
