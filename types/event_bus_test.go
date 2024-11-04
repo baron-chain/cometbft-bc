45bb4e0841a222888080e140a63eef7ec6c75393
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-04 23:22:23 +0200

    Update event_bus_test.go

--- types/event_bus_test.go
+++ types/event_bus_test.go
@@ -10,9 +10,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	abci "github.com/cometbft/cometbft/abci/types"
-	cmtpubsub "github.com/cometbft/cometbft/libs/pubsub"
-	cmtquery "github.com/cometbft/cometbft/libs/pubsub/query"
+	abci "github.com/baron-chain/cometbft-bc/abci/types"
+	cmtpubsub "github.com/baron-chain/cometbft-bc/libs/pubsub"
+	cmtquery "github.com/baron-chain/cometbft-bc/libs/pubsub/query"
 )
 
 func TestEventBusPublishEventTx(t *testing.T) {
