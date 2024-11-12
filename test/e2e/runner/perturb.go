74d8eace9d3c4defa6a4b308d3e7e50299ac3053
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-12 23:19:14 +0200

    Update perturb.go

--- test/e2e/runner/perturb.go
+++ test/e2e/runner/perturb.go
@@ -1,36 +1,33 @@
 package main
-// BC replace TBD
+
 import (
 	"fmt"
 	"time"
 
-	"github.com/cometbft/cometbft/libs/log"
-	rpctypes "github.com/cometbft/cometbft/rpc/core/types"
-	e2e "github.com/cometbft/cometbft/test/e2e/pkg"
+	"github.com/baron-chain/cometbft-bc/libs/log"
+	rpctypes "github.com/baron-chain/cometbft-bc/rpc/core/types"
+	e2e "github.com/baron-chain/cometbft-bc/test/e2e/pkg"
 )
 
-// Perturbs a running testnet.
-func Perturb(testnet *e2e.Testnet) error {
-	for _, node := range testnet.Nodes {
-		for _, perturbation := range node.Perturbations {
-			_, err := PerturbNode(node, perturbation)
-			if err != nil {
-				return err
-			}
-			time.Sleep(3 * time.Second) // give network some time to recover between each
-		}
-	}
-	return nil
+const (
+	perturbationRecoveryTime = 10 * time.Second
+	perturbationWaitTime     = 3 * time.Second
+	nodeRecoveryTimeout      = 20 * time.Second
+)
+
+type NodePerturber struct {
+	node     *e2e.Node
+	testnet  *e2e.Testnet
+	upgraded bool
+	name     string
 }
 
-// PerturbNode perturbs a node with a given perturbation, returning its status
-// after recovering.
-func PerturbNode(node *e2e.Node, perturbation e2e.Perturbation) (*rpctypes.ResultStatus, error) {
-	testnet := node.Testnet
-	out, err := execComposeOutput(testnet.Dir, "ps", "-q", node.Name)
+func NewNodePerturber(node *e2e.Node) (*NodePerturber, error) {
+	out, err := execComposeOutput(node.Testnet.Dir, "ps", "-q", node.Name)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to check node status: %w", err)
 	}
+
 	name := node.Name
 	upgraded := false
 	if len(out) == 0 {
@@ -41,77 +38,158 @@ func PerturbNode(node *e2e.Node, perturbation e2e.Perturbation) (*rpctypes.Resul
 				node.Name, name))
 	}
 
-	switch perturbation {
-	case e2e.PerturbationDisconnect:
-		logger.Info("perturb node", "msg", log.NewLazySprintf("Disconnecting node %v...", node.Name))
-		if err := execDocker("network", "disconnect", testnet.Name+"_"+testnet.Name, name); err != nil {
-			return nil, err
-		}
-		time.Sleep(10 * time.Second)
-		if err := execDocker("network", "connect", testnet.Name+"_"+testnet.Name, name); err != nil {
-			return nil, err
-		}
+	return &NodePerturber{
+		node:     node,
+		testnet:  node.Testnet,
+		upgraded: upgraded,
+		name:     name,
+	}, nil
+}
 
-	case e2e.PerturbationKill:
-		logger.Info("perturb node", "msg", log.NewLazySprintf("Killing node %v...", node.Name))
-		if err := execCompose(testnet.Dir, "kill", "-s", "SIGKILL", name); err != nil {
-			return nil, err
+func Perturb(testnet *e2e.Testnet) error {
+	for _, node := range testnet.Nodes {
+		if err := perturbNode(node); err != nil {
+			return fmt.Errorf("failed to perturb node %s: %w", node.Name, err)
 		}
-		if err := execCompose(testnet.Dir, "start", name); err != nil {
-			return nil, err
-		}
-
-	case e2e.PerturbationPause:
-		logger.Info("perturb node", "msg", log.NewLazySprintf("Pausing node %v...", node.Name))
-		if err := execCompose(testnet.Dir, "pause", name); err != nil {
-			return nil, err
-		}
-		time.Sleep(10 * time.Second)
-		if err := execCompose(testnet.Dir, "unpause", name); err != nil {
-			return nil, err
-		}
-
-	case e2e.PerturbationRestart:
-		logger.Info("perturb node", "msg", log.NewLazySprintf("Restarting node %v...", node.Name))
-		if err := execCompose(testnet.Dir, "restart", name); err != nil {
-			return nil, err
-		}
-
-	case e2e.PerturbationUpgrade:
-		oldV := node.Version
-		newV := node.Testnet.UpgradeVersion
-		if upgraded {
-			return nil, fmt.Errorf("node %v can't be upgraded twice from version '%v' to version '%v'",
-				node.Name, oldV, newV)
-		}
-		if oldV == newV {
-			logger.Info("perturb node", "msg",
-				log.NewLazySprintf("Skipping upgrade of node %v to version '%v'; versions are equal.",
-					node.Name, newV))
-			break
-		}
-		logger.Info("perturb node", "msg",
-			log.NewLazySprintf("Upgrading node %v from version '%v' to version '%v'...",
-				node.Name, oldV, newV))
-
-		if err := execCompose(testnet.Dir, "stop", name); err != nil {
-			return nil, err
-		}
-		time.Sleep(10 * time.Second)
-		if err := execCompose(testnet.Dir, "up", "-d", name+"_u"); err != nil {
-			return nil, err
-		}
-
-	default:
-		return nil, fmt.Errorf("unexpected perturbation %q", perturbation)
 	}
+	return nil
+}
 
-	status, err := waitForNode(node, 0, 20*time.Second)
+func perturbNode(node *e2e.Node) error {
+	for _, perturbation := range node.Perturbations {
+		if _, err := PerturbNode(node, perturbation); err != nil {
+			return err
+		}
+		time.Sleep(perturbationWaitTime)
+	}
+	return nil
+}
+
+func PerturbNode(node *e2e.Node, perturbation e2e.Perturbation) (*rpctypes.ResultStatus, error) {
+	perturber, err := NewNodePerturber(node)
 	if err != nil {
 		return nil, err
 	}
+
+	if err := perturber.applyPerturbation(perturbation); err != nil {
+		return nil, err
+	}
+
+	status, err := waitForNode(node, 0, nodeRecoveryTimeout)
+	if err != nil {
+		return nil, fmt.Errorf("node failed to recover: %w", err)
+	}
+
 	logger.Info("perturb node",
 		"msg",
 		log.NewLazySprintf("Node %v recovered at height %v", node.Name, status.SyncInfo.LatestBlockHeight))
+
 	return status, nil
+}
+
+func (p *NodePerturber) applyPerturbation(perturbation e2e.Perturbation) error {
+	switch perturbation {
+	case e2e.PerturbationDisconnect:
+		return p.handleDisconnect()
+	case e2e.PerturbationKill:
+		return p.handleKill()
+	case e2e.PerturbationPause:
+		return p.handlePause()
+	case e2e.PerturbationRestart:
+		return p.handleRestart()
+	case e2e.PerturbationUpgrade:
+		return p.handleUpgrade()
+	default:
+		return fmt.Errorf("unexpected perturbation %q", perturbation)
+	}
+}
+
+func (p *NodePerturber) handleDisconnect() error {
+	logger.Info("perturb node", "msg", log.NewLazySprintf("Disconnecting node %v...", p.node.Name))
+	
+	networkName := p.testnet.Name + "_" + p.testnet.Name
+	if err := execDocker("network", "disconnect", networkName, p.name); err != nil {
+		return fmt.Errorf("failed to disconnect network: %w", err)
+	}
+	
+	time.Sleep(perturbationRecoveryTime)
+	
+	if err := execDocker("network", "connect", networkName, p.name); err != nil {
+		return fmt.Errorf("failed to reconnect network: %w", err)
+	}
+	
+	return nil
+}
+
+func (p *NodePerturber) handleKill() error {
+	logger.Info("perturb node", "msg", log.NewLazySprintf("Killing node %v...", p.node.Name))
+	
+	if err := execCompose(p.testnet.Dir, "kill", "-s", "SIGKILL", p.name); err != nil {
+		return fmt.Errorf("failed to kill node: %w", err)
+	}
+	
+	if err := execCompose(p.testnet.Dir, "start", p.name); err != nil {
+		return fmt.Errorf("failed to start node: %w", err)
+	}
+	
+	return nil
+}
+
+func (p *NodePerturber) handlePause() error {
+	logger.Info("perturb node", "msg", log.NewLazySprintf("Pausing node %v...", p.node.Name))
+	
+	if err := execCompose(p.testnet.Dir, "pause", p.name); err != nil {
+		return fmt.Errorf("failed to pause node: %w", err)
+	}
+	
+	time.Sleep(perturbationRecoveryTime)
+	
+	if err := execCompose(p.testnet.Dir, "unpause", p.name); err != nil {
+		return fmt.Errorf("failed to unpause node: %w", err)
+	}
+	
+	return nil
+}
+
+func (p *NodePerturber) handleRestart() error {
+	logger.Info("perturb node", "msg", log.NewLazySprintf("Restarting node %v...", p.node.Name))
+	
+	if err := execCompose(p.testnet.Dir, "restart", p.name); err != nil {
+		return fmt.Errorf("failed to restart node: %w", err)
+	}
+	
+	return nil
+}
+
+func (p *NodePerturber) handleUpgrade() error {
+	oldVersion := p.node.Version
+	newVersion := p.testnet.UpgradeVersion
+
+	if p.upgraded {
+		return fmt.Errorf("node %v can't be upgraded twice from version '%v' to version '%v'",
+			p.node.Name, oldVersion, newVersion)
+	}
+
+	if oldVersion == newVersion {
+		logger.Info("perturb node", "msg",
+			log.NewLazySprintf("Skipping upgrade of node %v to version '%v'; versions are equal.",
+				p.node.Name, newVersion))
+		return nil
+	}
+
+	logger.Info("perturb node", "msg",
+		log.NewLazySprintf("Upgrading node %v from version '%v' to version '%v'...",
+			p.node.Name, oldVersion, newVersion))
+
+	if err := execCompose(p.testnet.Dir, "stop", p.name); err != nil {
+		return fmt.Errorf("failed to stop node: %w", err)
+	}
+
+	time.Sleep(perturbationRecoveryTime)
+
+	if err := execCompose(p.testnet.Dir, "up", "-d", p.name+"_u"); err != nil {
+		return fmt.Errorf("failed to start upgraded node: %w", err)
+	}
+
+	return nil
 }
