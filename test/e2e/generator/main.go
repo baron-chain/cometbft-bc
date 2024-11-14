1c218e6b8d9de49f4e37599e897dfcc8fb22fb9a
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-14 23:38:02 +0200

    Update main.go

--- test/e2e/generator/main.go
+++ test/e2e/generator/main.go
@@ -3,111 +3,152 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/rand"
 	"os"
 	"path/filepath"
 
+	"github.com/baron-chain/cometbft-bc/crypto/rand"
+	"github.com/baron-chain/cometbft-bc/libs/log"
+	bcconfig "github.com/baron-chain/cometbft-bc/config"
 	"github.com/spf13/cobra"
-
-	"github.com/cometbft/cometbft/libs/log"
 )
 
 const (
-	randomSeed int64 = 4827085738
+	defaultSeed = 4827085738
+	appName     = "baron-generator"
 )
 
-var logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+var (
+	logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+	config = bcconfig.DefaultConfig()
+)
 
-func main() {
-	NewCLI().Run()
+type Generator struct {
+	cmd    *cobra.Command
+	config *GeneratorConfig
 }
 
-// CLI is the Cobra-based command-line interface.
-type CLI struct {
-	root *cobra.Command
+type GeneratorConfig struct {
+	outputDir     string
+	groups        int
+	versions      string
+	metrics       bool
+	rand          *rand.Rand
 }
 
-// NewCLI sets up the CLI.
-func NewCLI() *CLI {
-	cli := &CLI{}
-	cli.root = &cobra.Command{
-		Use:           "generator -d dir [-g int] [-m version_weight_csv] [-p]",
-		Short:         "End-to-end testnet generator",
-		SilenceUsage:  true,
-		SilenceErrors: true, // we'll output them ourselves in Run()
-		RunE: func(cmd *cobra.Command, args []string) error {
-			dir, err := cmd.Flags().GetString("dir")
-			if err != nil {
-				return err
-			}
-			groups, err := cmd.Flags().GetInt("groups")
-			if err != nil {
-				return err
-			}
-			multiVersion, err := cmd.Flags().GetString("multi-version")
-			if err != nil {
-				return err
-			}
-			prometheus, err := cmd.Flags().GetBool("prometheus")
-			if err != nil {
-				return err
-			}
-			return cli.generate(dir, groups, multiVersion, prometheus)
+func NewGenerator() *Generator {
+	g := &Generator{
+		config: &GeneratorConfig{
+			rand: rand.NewRand(),
 		},
 	}
-
-	cli.root.PersistentFlags().StringP("dir", "d", "", "Output directory for manifests")
-	_ = cli.root.MarkPersistentFlagRequired("dir")
-	cli.root.PersistentFlags().StringP("multi-version", "m", "", "Comma-separated list of versions of CometBFT to test in the generated testnets, "+
-		"or empty to only use this branch's version")
-	cli.root.PersistentFlags().IntP("groups", "g", 0, "Number of groups")
-	cli.root.PersistentFlags().BoolP("prometheus", "p", false, "Enable generation of Prometheus metrics on all manifests")
-
-	return cli
+	g.setupCommands()
+	return g
 }
 
-// generate generates manifests in a directory.
-func (cli *CLI) generate(dir string, groups int, multiVersion string, prometheus bool) error {
-	err := os.MkdirAll(dir, 0o755)
-	if err != nil {
-		return err
+func (g *Generator) setupCommands() {
+	g.cmd = &cobra.Command{
+		Use:   fmt.Sprintf("%s --dir <output-dir> [flags]", appName),
+		Short: "Baron Chain testnet generator",
+		Long: `Generates Baron Chain testnet configurations for E2E testing.
+Supports multi-version testing and Prometheus metrics integration.`,
+		RunE: g.runGenerate,
 	}
 
-	cfg := &generateConfig{
-		randSource:   rand.New(rand.NewSource(randomSeed)), //nolint:gosec
-		multiVersion: multiVersion,
-		prometheus:   prometheus,
+	flags := g.cmd.PersistentFlags()
+	flags.StringVarP(&g.config.outputDir, "dir", "d", "", "Output directory for testnet configs")
+	flags.IntVarP(&g.config.groups, "groups", "g", 0, "Number of testnet groups")
+	flags.StringVarP(&g.config.versions, "versions", "v", "", 
+		"Comma-separated Baron Chain versions to test (empty = current version)")
+	flags.BoolVarP(&g.config.metrics, "metrics", "m", false, "Enable Prometheus metrics")
+
+	g.cmd.MarkPersistentFlagRequired("dir")
+}
+
+func (g *Generator) runGenerate(cmd *cobra.Command, args []string) error {
+	if err := g.validateConfig(); err != nil {
+		return fmt.Errorf("config validation failed: %w", err)
 	}
-	manifests, err := Generate(cfg)
+
+	logger.Info("generating testnet configurations", 
+		"dir", g.config.outputDir,
+		"groups", g.config.groups,
+		"metrics", g.config.metrics)
+
+	manifests, err := g.generateManifests()
 	if err != nil {
-		return err
+		return fmt.Errorf("manifest generation failed: %w", err)
 	}
-	if groups <= 0 {
-		for i, manifest := range manifests {
-			err = manifest.Save(filepath.Join(dir, fmt.Sprintf("gen-%04d.toml", i)))
-			if err != nil {
-				return err
-			}
+
+	return g.saveManifests(manifests)
+}
+
+func (g *Generator) validateConfig() error {
+	if err := os.MkdirAll(g.config.outputDir, 0755); err != nil {
+		return fmt.Errorf("failed to create output directory: %w", err)
+	}
+
+	if g.config.groups < 0 {
+		return fmt.Errorf("invalid group count: %d", g.config.groups)
+	}
+
+	return nil
+}
+
+func (g *Generator) generateManifests() ([]Manifest, error) {
+	cfg := &ManifestConfig{
+		Versions:    g.config.versions,
+		EnableMetrics: g.config.metrics,
+		ChainID:     fmt.Sprintf("baron-test-%d", g.config.rand.Int63()),
+	}
+
+	manifests, err := GenerateManifests(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate manifests: %w", err)
+	}
+
+	return manifests, nil
+}
+
+func (g *Generator) saveManifests(manifests []Manifest) error {
+	if g.config.groups <= 0 {
+		return g.saveFlatManifests(manifests)
+	}
+	return g.saveGroupedManifests(manifests)
+}
+
+func (g *Generator) saveFlatManifests(manifests []Manifest) error {
+	for i, m := range manifests {
+		filename := filepath.Join(g.config.outputDir, fmt.Sprintf("baron-net-%04d.toml", i))
+		if err := m.Save(filename); err != nil {
+			return fmt.Errorf("failed to save manifest %d: %w", i, err)
 		}
-	} else {
-		groupSize := int(math.Ceil(float64(len(manifests)) / float64(groups)))
-		for g := 0; g < groups; g++ {
-			for i := 0; i < groupSize && g*groupSize+i < len(manifests); i++ {
-				manifest := manifests[g*groupSize+i]
-				err = manifest.Save(filepath.Join(dir, fmt.Sprintf("gen-group%02d-%04d.toml", g, i)))
-				if err != nil {
-					return err
-				}
+		logger.Info("saved manifest", "file", filename)
+	}
+	return nil
+}
+
+func (g *Generator) saveGroupedManifests(manifests []Manifest) error {
+	groupSize := int(math.Ceil(float64(len(manifests)) / float64(g.config.groups)))
+
+	for group := 0; group < g.config.groups; group++ {
+		for i := 0; i < groupSize && group*groupSize+i < len(manifests); i++ {
+			manifest := manifests[group*groupSize+i]
+			filename := filepath.Join(g.config.outputDir, 
+				fmt.Sprintf("baron-net-g%02d-%04d.toml", group, i))
+			
+			if err := manifest.Save(filename); err != nil {
+				return fmt.Errorf("failed to save manifest group %d-%d: %w", group, i, err)
 			}
+			logger.Info("saved manifest", "group", group, "file", filename)
 		}
 	}
 	return nil
 }
 
-// Run runs the CLI.
-func (cli *CLI) Run() {
-	if err := cli.root.Execute(); err != nil {
-		logger.Error(err.Error())
+func main() {
+	generator := NewGenerator()
+	if err := generator.cmd.Execute(); err != nil {
+		logger.Error("generator failed", "err", err)
 		os.Exit(1)
 	}
 }
