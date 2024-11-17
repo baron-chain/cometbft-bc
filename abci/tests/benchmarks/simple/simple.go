7ad386131704791c039f6e18ec5996ead876ac89
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-17 02:07:20 +0200

    Update simple.go

--- abci/tests/benchmarks/simple/simple.go
+++ abci/tests/benchmarks/simple/simple.go
@@ -1,69 +1,138 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"reflect"
+    "bufio"
+    "context"
+    "fmt"
+    "io"
+    "log"
+    "reflect"
+    "time"
 
-	"github.com/cometbft/cometbft/abci/types"
-	cmtnet "github.com/cometbft/cometbft/libs/net"
+    "github.com/baron-chain/cometbft-bc/abci/types"
+    bcnet "github.com/baron-chain/cometbft-bc/libs/net"
 )
 
-func main() {
+const (
+    defaultSocketPath = "unix://test.sock"
+    logInterval      = 1000
+    bufferSize       = 1024 * 1024 // 1MB
+    timeout          = 30 * time.Second
+)
 
-	conn, err := cmtnet.Connect("unix://test.sock")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// Make a bunch of requests
-	counter := 0
-	for i := 0; ; i++ {
-		req := types.ToRequestEcho("foobar")
-		_, err := makeRequest(conn, req)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		counter++
-		if counter%1000 == 0 {
-			fmt.Println(counter)
-		}
-	}
+type BCClient struct {
+    conn   io.ReadWriter
+    writer *bufio.Writer
+    ctx    context.Context
+    cancel context.CancelFunc
 }
 
-func makeRequest(conn io.ReadWriter, req *types.Request) (*types.Response, error) {
-	var bufWriter = bufio.NewWriter(conn)
+func newBCClient(socketPath string) (*BCClient, error) {
+    conn, err := bcnet.Connect(socketPath)
+    if err != nil {
+        return nil, fmt.Errorf("baron chain connection failed: %w", err)
+    }
 
-	// Write desired request
-	err := types.WriteMessage(req, bufWriter)
-	if err != nil {
-		return nil, err
-	}
-	err = types.WriteMessage(types.ToRequestFlush(), bufWriter)
-	if err != nil {
-		return nil, err
-	}
-	err = bufWriter.Flush()
-	if err != nil {
-		return nil, err
-	}
+    ctx, cancel := context.WithTimeout(context.Background(), timeout)
 
-	// Read desired response
-	var res = &types.Response{}
-	err = types.ReadMessage(conn, res)
-	if err != nil {
-		return nil, err
-	}
-	var resFlush = &types.Response{}
-	err = types.ReadMessage(conn, resFlush)
-	if err != nil {
-		return nil, err
-	}
-	if _, ok := resFlush.Value.(*types.Response_Flush); !ok {
-		return nil, fmt.Errorf("expected flush response but got something else: %v", reflect.TypeOf(resFlush))
-	}
+    return &BCClient{
+        conn:   conn,
+        writer: bufio.NewWriterSize(conn, bufferSize),
+        ctx:    ctx,
+        cancel: cancel,
+    }, nil
+}
 
-	return res, nil
+func (c *BCClient) close() {
+    c.cancel()
+    if closer, ok := c.conn.(io.Closer); ok {
+        closer.Close()
+    }
+}
+
+func (c *BCClient) makeRequest(req *types.Request) (*types.Response, error) {
+    // Write request with flush
+    if err := c.writeRequest(req); err != nil {
+        return nil, fmt.Errorf("baron chain request write failed: %w", err)
+    }
+
+    // Read response with flush verification
+    return c.readResponse()
+}
+
+func (c *BCClient) writeRequest(req *types.Request) error {
+    // Write main request
+    if err := types.WriteMessage(req, c.writer); err != nil {
+        return fmt.Errorf("request write failed: %w", err)
+    }
+
+    // Write flush request
+    if err := types.WriteMessage(types.ToRequestFlush(), c.writer); err != nil {
+        return fmt.Errorf("flush request write failed: %w", err)
+    }
+
+    // Flush writer
+    if err := c.writer.Flush(); err != nil {
+        return fmt.Errorf("buffer flush failed: %w", err)
+    }
+
+    return nil
+}
+
+func (c *BCClient) readResponse() (*types.Response, error) {
+    // Read main response
+    response := &types.Response{}
+    if err := types.ReadMessage(c.conn, response); err != nil {
+        return nil, fmt.Errorf("response read failed: %w", err)
+    }
+
+    // Read and verify flush response
+    flushResponse := &types.Response{}
+    if err := types.ReadMessage(c.conn, flushResponse); err != nil {
+        return nil, fmt.Errorf("flush response read failed: %w", err)
+    }
+
+    if _, ok := flushResponse.Value.(*types.Response_Flush); !ok {
+        return nil, fmt.Errorf("unexpected flush response type: %v", reflect.TypeOf(flushResponse))
+    }
+
+    return response, nil
+}
+
+func main() {
+    // Initialize client
+    client, err := newBCClient(defaultSocketPath)
+    if err != nil {
+        log.Fatalf("Baron Chain client initialization failed: %v", err)
+    }
+    defer client.close()
+
+    // Process requests
+    var counter int
+
+    for {
+        select {
+        case <-client.ctx.Done():
+            log.Printf("Baron Chain client timeout after %d requests", counter)
+            return
+        default:
+            // Create and send echo request
+            req := types.ToRequestEcho("baron-chain-test")
+            resp, err := client.makeRequest(req)
+            if err != nil {
+                log.Fatalf("Baron Chain request failed at count %d: %v", counter, err)
+            }
+
+            // Process response if needed
+            if _, ok := resp.Value.(*types.Response_Echo); !ok {
+                log.Fatalf("Unexpected response type at count %d: %v", counter, reflect.TypeOf(resp))
+            }
+
+            // Log progress
+            counter++
+            if counter%logInterval == 0 {
+                fmt.Printf("Processed %d Baron Chain requests\n", counter)
+            }
+        }
+    }
 }
