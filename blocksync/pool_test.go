3968ecf1794ed4ebf4783126291637197a599d3f
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-20 21:09:05 +0200

    Update pool_test.go

--- blocksync/pool_test.go
+++ blocksync/pool_test.go
@@ -1,242 +1,200 @@
 package blocksync
 
 import (
-	"fmt"
-	"testing"
-	"time"
+   "fmt"
+   "testing"
+   "time"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+   "github.com/stretchr/testify/assert"
+   "github.com/stretchr/testify/require"
 
-	"github.com/cometbft/cometbft/libs/log"
-	cmtrand "github.com/cometbft/cometbft/libs/rand"
-	"github.com/cometbft/cometbft/p2p"
-	"github.com/cometbft/cometbft/types"
+   "github.com/baron-chain/cometbft-bc/libs/log"
+   cmtrand "github.com/baron-chain/cometbft-bc/libs/rand"
+   "github.com/baron-chain/cometbft-bc/p2p"
+   "github.com/baron-chain/cometbft-bc/types"
 )
 
 func init() {
-	peerTimeout = 2 * time.Second
+   peerTimeout = 2 * time.Second
 }
 
 type testPeer struct {
-	id        p2p.ID
-	base      int64
-	height    int64
-	inputChan chan inputData // make sure each peer's data is sequential
+   id        p2p.ID
+   base      int64
+   height    int64
+   inputChan chan inputData
 }
 
 type inputData struct {
-	t       *testing.T
-	pool    *BlockPool
-	request BlockRequest
+   t       *testing.T
+   pool    *BlockPool
+   request BlockRequest
 }
 
 func (p testPeer) runInputRoutine() {
-	go func() {
-		for input := range p.inputChan {
-			p.simulateInput(input)
-		}
-	}()
-}
-
-// Request desired, pretend like we got the block immediately.
-func (p testPeer) simulateInput(input inputData) {
-	block := &types.Block{Header: types.Header{Height: input.request.Height}}
-	input.pool.AddBlock(input.request.PeerID, block, 123)
-	// TODO: uncommenting this creates a race which is detected by:
-	// https://github.com/golang/go/blob/2bd767b1022dd3254bcec469f0ee164024726486/src/testing/testing.go#L854-L856
-	// see: https://github.com/tendermint/tendermint/issues/3390#issue-418379890
-	// input.t.Logf("Added block from peer %v (height: %v)", input.request.PeerID, input.request.Height)
+   go func() {
+       for input := range p.inputChan {
+           block := &types.Block{Header: types.Header{Height: input.request.Height}}
+           input.pool.AddBlock(input.request.PeerID, block, 123)
+       }
+   }()
 }
 
 type testPeers map[p2p.ID]testPeer
 
 func (ps testPeers) start() {
-	for _, v := range ps {
-		v.runInputRoutine()
-	}
+   for _, p := range ps {
+       p.runInputRoutine()
+   }
 }
 
 func (ps testPeers) stop() {
-	for _, v := range ps {
-		close(v.inputChan)
-	}
+   for _, p := range ps {
+       close(p.inputChan)
+   }
 }
 
 func makePeers(numPeers int, minHeight, maxHeight int64) testPeers {
-	peers := make(testPeers, numPeers)
-	for i := 0; i < numPeers; i++ {
-		peerID := p2p.ID(cmtrand.Str(12))
-		height := minHeight + cmtrand.Int63n(maxHeight-minHeight)
-		base := minHeight + int64(i)
-		if base > height {
-			base = height
-		}
-		peers[peerID] = testPeer{peerID, base, height, make(chan inputData, 10)}
-	}
-	return peers
+   peers := make(testPeers, numPeers)
+   for i := 0; i < numPeers; i++ {
+       peerID := p2p.ID(cmtrand.Str(12))
+       height := minHeight + cmtrand.Int63n(maxHeight-minHeight)
+       base := minHeight + int64(i)
+       if base > height {
+           base = height
+       }
+       peers[peerID] = testPeer{
+           id:        peerID,
+           base:      base,
+           height:    height,
+           inputChan: make(chan inputData, 10),
+       }
+   }
+   return peers
 }
 
 func TestBlockPoolBasic(t *testing.T) {
-	start := int64(42)
-	peers := makePeers(10, start+1, 1000)
-	errorsCh := make(chan peerError, 1000)
-	requestsCh := make(chan BlockRequest, 1000)
-	pool := NewBlockPool(start, requestsCh, errorsCh)
-	pool.SetLogger(log.TestingLogger())
+   const startHeight = 42
+   peers := makePeers(10, startHeight+1, 1000)
+   errorsCh := make(chan peerError, 1000)
+   requestsCh := make(chan BlockRequest, 1000)
+   
+   pool := NewBlockPool(startHeight, requestsCh, errorsCh)
+   pool.SetLogger(log.TestingLogger())
+   require.NoError(t, pool.Start())
+   
+   t.Cleanup(func() {
+       require.NoError(t, pool.Stop())
+   })
 
-	err := pool.Start()
-	if err != nil {
-		t.Error(err)
-	}
+   peers.start()
+   defer peers.stop()
 
-	t.Cleanup(func() {
-		if err := pool.Stop(); err != nil {
-			t.Error(err)
-		}
-	})
+   go func() {
+       for _, peer := range peers {
+           pool.SetPeerRange(peer.id, peer.base, peer.height)
+       }
+   }()
 
-	peers.start()
-	defer peers.stop()
+   go func() {
+       for pool.IsRunning() {
+           if first, second := pool.PeekTwoBlocks(); first != nil && second != nil {
+               pool.PopRequest()
+           } else {
+               time.Sleep(time.Second)
+           }
+       }
+   }()
 
-	// Introduce each peer.
-	go func() {
-		for _, peer := range peers {
-			pool.SetPeerRange(peer.id, peer.base, peer.height)
-		}
-	}()
-
-	// Start a goroutine to pull blocks
-	go func() {
-		for {
-			if !pool.IsRunning() {
-				return
-			}
-			first, second := pool.PeekTwoBlocks()
-			if first != nil && second != nil {
-				pool.PopRequest()
-			} else {
-				time.Sleep(1 * time.Second)
-			}
-		}
-	}()
-
-	// Pull from channels
-	for {
-		select {
-		case err := <-errorsCh:
-			t.Error(err)
-		case request := <-requestsCh:
-			t.Logf("Pulled new BlockRequest %v", request)
-			if request.Height == 300 {
-				return // Done!
-			}
-
-			peers[request.PeerID].inputChan <- inputData{t, pool, request}
-		}
-	}
+   for {
+       select {
+       case err := <-errorsCh:
+           t.Error(err)
+       case request := <-requestsCh:
+           if request.Height == 300 {
+               return
+           }
+           peers[request.PeerID].inputChan <- inputData{t, pool, request}
+       }
+   }
 }
 
 func TestBlockPoolTimeout(t *testing.T) {
-	start := int64(42)
-	peers := makePeers(10, start+1, 1000)
-	errorsCh := make(chan peerError, 1000)
-	requestsCh := make(chan BlockRequest, 1000)
-	pool := NewBlockPool(start, requestsCh, errorsCh)
-	pool.SetLogger(log.TestingLogger())
-	err := pool.Start()
-	if err != nil {
-		t.Error(err)
-	}
-	t.Cleanup(func() {
-		if err := pool.Stop(); err != nil {
-			t.Error(err)
-		}
-	})
+   const startHeight = 42
+   peers := makePeers(10, startHeight+1, 1000)
+   errorsCh := make(chan peerError, 1000)
+   requestsCh := make(chan BlockRequest, 1000)
+   
+   pool := NewBlockPool(startHeight, requestsCh, errorsCh)
+   pool.SetLogger(log.TestingLogger())
+   require.NoError(t, pool.Start())
+   
+   t.Cleanup(func() {
+       require.NoError(t, pool.Stop())
+   })
 
-	for _, peer := range peers {
-		t.Logf("Peer %v", peer.id)
-	}
+   go func() {
+       for _, peer := range peers {
+           pool.SetPeerRange(peer.id, peer.base, peer.height)
+       }
+   }()
 
-	// Introduce each peer.
-	go func() {
-		for _, peer := range peers {
-			pool.SetPeerRange(peer.id, peer.base, peer.height)
-		}
-	}()
+   go func() {
+       for pool.IsRunning() {
+           if first, second := pool.PeekTwoBlocks(); first != nil && second != nil {
+               pool.PopRequest()
+           } else {
+               time.Sleep(time.Second)
+           }
+       }
+   }()
 
-	// Start a goroutine to pull blocks
-	go func() {
-		for {
-			if !pool.IsRunning() {
-				return
-			}
-			first, second := pool.PeekTwoBlocks()
-			if first != nil && second != nil {
-				pool.PopRequest()
-			} else {
-				time.Sleep(1 * time.Second)
-			}
-		}
-	}()
-
-	// Pull from channels
-	counter := 0
-	timedOut := map[p2p.ID]struct{}{}
-	for {
-		select {
-		case err := <-errorsCh:
-			t.Log(err)
-			// consider error to be always timeout here
-			if _, ok := timedOut[err.peerID]; !ok {
-				counter++
-				if counter == len(peers) {
-					return // Done!
-				}
-			}
-		case request := <-requestsCh:
-			t.Logf("Pulled new BlockRequest %+v", request)
-		}
-	}
+   timedOutPeers := make(map[p2p.ID]struct{})
+   for timeouts := 0; timeouts < len(peers); {
+       select {
+       case err := <-errorsCh:
+           if _, ok := timedOutPeers[err.peerID]; !ok {
+               timedOutPeers[err.peerID] = struct{}{}
+               timeouts++
+           }
+       case request := <-requestsCh:
+           t.Logf("Received request: %+v", request)
+       }
+   }
 }
 
 func TestBlockPoolRemovePeer(t *testing.T) {
-	peers := make(testPeers, 10)
-	for i := 0; i < 10; i++ {
-		peerID := p2p.ID(fmt.Sprintf("%d", i+1))
-		height := int64(i + 1)
-		peers[peerID] = testPeer{peerID, 0, height, make(chan inputData)}
-	}
-	requestsCh := make(chan BlockRequest)
-	errorsCh := make(chan peerError)
+   peers := make(testPeers, 10)
+   for i := 0; i < 10; i++ {
+       peerID := p2p.ID(fmt.Sprintf("%d", i+1))
+       peers[peerID] = testPeer{
+           id:        peerID,
+           base:      0,
+           height:    int64(i + 1),
+           inputChan: make(chan inputData),
+       }
+   }
 
-	pool := NewBlockPool(1, requestsCh, errorsCh)
-	pool.SetLogger(log.TestingLogger())
-	err := pool.Start()
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		if err := pool.Stop(); err != nil {
-			t.Error(err)
-		}
-	})
+   pool := NewBlockPool(1, make(chan BlockRequest), make(chan peerError))
+   pool.SetLogger(log.TestingLogger())
+   require.NoError(t, pool.Start())
+   
+   t.Cleanup(func() {
+       require.NoError(t, pool.Stop())
+   })
 
-	// add peers
-	for peerID, peer := range peers {
-		pool.SetPeerRange(peerID, peer.base, peer.height)
-	}
-	assert.EqualValues(t, 10, pool.MaxPeerHeight())
+   for id, peer := range peers {
+       pool.SetPeerRange(id, peer.base, peer.height)
+   }
+   assert.Equal(t, int64(10), pool.MaxPeerHeight())
 
-	// remove not-existing peer
-	assert.NotPanics(t, func() { pool.RemovePeer(p2p.ID("Superman")) })
+   assert.NotPanics(t, func() { pool.RemovePeer("non-existing-peer") })
 
-	// remove peer with biggest height
-	pool.RemovePeer(p2p.ID("10"))
-	assert.EqualValues(t, 9, pool.MaxPeerHeight())
+   pool.RemovePeer("10")
+   assert.Equal(t, int64(9), pool.MaxPeerHeight())
 
-	// remove all peers
-	for peerID := range peers {
-		pool.RemovePeer(peerID)
-	}
-
-	assert.EqualValues(t, 0, pool.MaxPeerHeight())
+   for id := range peers {
+       pool.RemovePeer(id)
+   }
+   assert.Equal(t, int64(0), pool.MaxPeerHeight())
 }
