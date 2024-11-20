8c5d0256cb056aa81059d5cb2b7cff9faa77ca6c
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-20 21:04:27 +0200

    Update msgs_test.go

--- blocksync/msgs_test.go
+++ blocksync/msgs_test.go
@@ -5,7 +5,7 @@ import (
     "math"
     "testing"
 
-    "github.com/baron-chain/gogoproto/proto"
+    "github.com/baron-chain/gogoproto-bc/proto"
     "github.com/stretchr/testify/assert"
     "github.com/stretchr/testify/require"
 
