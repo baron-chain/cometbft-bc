8b83d0a0c298370fa878319c0374f618111d5c91
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-24 23:38:57 +0200

    Update height_vote_set_test.go

--- consensus/types/height_vote_set_test.go
+++ consensus/types/height_vote_set_test.go
@@ -1,87 +1,128 @@
 package types
 
 import (
-	"fmt"
-	"os"
-	"testing"
+    "fmt"
+    "os"
+    "testing"
 
-	cfg "github.com/cometbft/cometbft/config"
-	"github.com/cometbft/cometbft/crypto/tmhash"
-	cmtrand "github.com/cometbft/cometbft/libs/rand"
-	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
-	"github.com/cometbft/cometbft/types"
-	cmttime "github.com/cometbft/cometbft/types/time"
+    cfg "github.com/baron-chain/cometbft-bc/config"
+    "github.com/baron-chain/cometbft-bc/crypto/tmhash"
+    bcrand "github.com/baron-chain/cometbft-bc/libs/rand"
+    bcproto "github.com/baron-chain/cometbft-bc/proto/tendermint/types"
+    "github.com/baron-chain/cometbft-bc/types"
+    bctime "github.com/baron-chain/cometbft-bc/types/time"
 )
 
-var config *cfg.Config // NOTE: must be reset for each _test.go file
+var (
+    testConfig *cfg.Config
+    testChainID string = "baron_test_chain"
+)
 
 func TestMain(m *testing.M) {
-	config = cfg.ResetTestRoot("consensus_height_vote_set_test")
-	code := m.Run()
-	os.RemoveAll(config.RootDir)
-	os.Exit(code)
+    testConfig = cfg.ResetTestRoot("baron_consensus_vote_test")
+    exitCode := m.Run()
+    cleanup()
+    os.Exit(exitCode)
+}
+
+func cleanup() {
+    if testConfig.RootDir != "" {
+        os.RemoveAll(testConfig.RootDir)
+    }
 }
 
 func TestPeerCatchupRounds(t *testing.T) {
-	valSet, privVals := types.RandValidatorSet(10, 1)
+    const (
+        numValidators = 10
+        votingPower   = 1
+        height        = int64(1)
+        peerID1       = "peer1"
+        peerID2       = "peer2"
+    )
 
-	hvs := NewHeightVoteSet(config.ChainID(), 1, valSet)
+    testCases := []struct {
+        name        string
+        round       int32
+        peerID      string
+        expectAdd   bool
+        expectError error
+    }{
+        {
+            name:        "first catchup round",
+            round:       999,
+            peerID:      peerID1,
+            expectAdd:   true,
+            expectError: nil,
+        },
+        {
+            name:        "second catchup round",
+            round:       1000,
+            peerID:      peerID1,
+            expectAdd:   true,
+            expectError: nil,
+        },
+        {
+            name:        "exceed catchup limit",
+            round:       1001,
+            peerID:      peerID1,
+            expectAdd:   false,
+            expectError: ErrUnwantedRoundVote,
+        },
+        {
+            name:        "different peer valid round",
+            round:       1001,
+            peerID:      peerID2,
+            expectAdd:   true,
+            expectError: nil,
+        },
+    }
 
-	vote999_0 := makeVoteHR(t, 1, 0, 999, privVals)
-	added, err := hvs.AddVote(vote999_0, "peer1")
-	if !added || err != nil {
-		t.Error("Expected to successfully add vote from peer", added, err)
-	}
+    valSet, privVals := types.RandValidatorSet(numValidators, votingPower)
+    hvs := NewHeightVoteSet(testChainID, height, valSet)
 
-	vote1000_0 := makeVoteHR(t, 1, 0, 1000, privVals)
-	added, err = hvs.AddVote(vote1000_0, "peer1")
-	if !added || err != nil {
-		t.Error("Expected to successfully add vote from peer", added, err)
-	}
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T) {
+            vote := generateTestVote(t, height, 0, tc.round, privVals)
+            added, err := hvs.AddVote(vote, tc.peerID)
 
-	vote1001_0 := makeVoteHR(t, 1, 0, 1001, privVals)
-	added, err = hvs.AddVote(vote1001_0, "peer1")
-	if err != ErrGotVoteFromUnwantedRound {
-		t.Errorf("expected GotVoteFromUnwantedRoundError, but got %v", err)
-	}
-	if added {
-		t.Error("Expected to *not* add vote from peer, too many catchup rounds.")
-	}
-
-	added, err = hvs.AddVote(vote1001_0, "peer2")
-	if !added || err != nil {
-		t.Error("Expected to successfully add vote from another peer")
-	}
-
+            if added != tc.expectAdd {
+                t.Errorf("expected AddVote to return %v, got %v", tc.expectAdd, added)
+            }
+            
+            if err != tc.expectError {
+                t.Errorf("expected error %v, got %v", tc.expectError, err)
+            }
+        })
+    }
 }
 
-func makeVoteHR(t *testing.T, height int64, valIndex, round int32, privVals []types.PrivValidator) *types.Vote {
-	privVal := privVals[valIndex]
-	pubKey, err := privVal.GetPubKey()
-	if err != nil {
-		panic(err)
-	}
+func generateTestVote(t *testing.T, height int64, valIndex, round int32, privVals []types.PrivValidator) *types.Vote {
+    t.Helper()
+    
+    privVal := privVals[valIndex]
+    pubKey, err := privVal.GetPubKey()
+    if err != nil {
+        t.Fatalf("failed to get pubkey: %v", err)
+    }
 
-	randBytes := cmtrand.Bytes(tmhash.Size)
+    vote := &types.Vote{
+        ValidatorAddress: pubKey.Address(),
+        ValidatorIndex:   valIndex,
+        Height:          height,
+        Round:           round,
+        Timestamp:       bctime.Now(),
+        Type:            bcproto.PrecommitType,
+        BlockID: types.BlockID{
+            Hash:           bcrand.Bytes(tmhash.Size),
+            PartSetHeader: types.PartSetHeader{},
+        },
+    }
 
-	vote := &types.Vote{
-		ValidatorAddress: pubKey.Address(),
-		ValidatorIndex:   valIndex,
-		Height:           height,
-		Round:            round,
-		Timestamp:        cmttime.Now(),
-		Type:             cmtproto.PrecommitType,
-		BlockID:          types.BlockID{Hash: randBytes, PartSetHeader: types.PartSetHeader{}},
-	}
-	chainID := config.ChainID()
-
-	v := vote.ToProto()
-	err = privVal.SignVote(chainID, v)
-	if err != nil {
-		panic(fmt.Sprintf("Error signing vote: %v", err))
-	}
-
-	vote.Signature = v.Signature
-
-	return vote
+    v := vote.ToProto()
+    if err := privVal.SignVote(testChainID, v); err != nil {
+        t.Fatalf("failed to sign vote: %v", err)
+    }
+    
+    vote.Signature = v.Signature
+    return vote
 }
