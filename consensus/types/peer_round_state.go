f1029fe1bf737ce2550cb2b7cc0ae6a18c8d6dc0
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-11-24 23:40:05 +0200

    Update peer_round_state.go

--- consensus/types/peer_round_state.go
+++ consensus/types/peer_round_state.go
@@ -1,68 +1,109 @@
 package types
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/cometbft/cometbft/libs/bits"
-	"github.com/cometbft/cometbft/types"
+    "fmt"
+    "time"
+    
+    "github.com/baron-chain/cometbft-bc/libs/bits"
+    "github.com/baron-chain/cometbft-bc/types"
 )
 
-//-----------------------------------------------------------------------------
-
-// PeerRoundState contains the known state of a peer.
-// NOTE: Read-only when returned by PeerState.GetRoundState().
+// PeerRoundState represents a peer's known state in the consensus protocol
 type PeerRoundState struct {
-	Height int64         `json:"height"` // Height peer is at
-	Round  int32         `json:"round"`  // Round peer is at, -1 if unknown.
-	Step   RoundStepType `json:"step"`   // Step peer is at
-
-	// Estimated start of round 0 at this height
-	StartTime time.Time `json:"start_time"`
-
-	// True if peer has proposal for this round
-	Proposal                   bool                `json:"proposal"`
-	ProposalBlockPartSetHeader types.PartSetHeader `json:"proposal_block_part_set_header"`
-	ProposalBlockParts         *bits.BitArray      `json:"proposal_block_parts"`
-	// Proposal's POL round. -1 if none.
-	ProposalPOLRound int32 `json:"proposal_pol_round"`
-
-	// nil until ProposalPOLMessage received.
-	ProposalPOL     *bits.BitArray `json:"proposal_pol"`
-	Prevotes        *bits.BitArray `json:"prevotes"`          // All votes peer has for this round
-	Precommits      *bits.BitArray `json:"precommits"`        // All precommits peer has for this round
-	LastCommitRound int32          `json:"last_commit_round"` // Round of commit for last height. -1 if none.
-	LastCommit      *bits.BitArray `json:"last_commit"`       // All commit precommits of commit for last height.
-
-	// Round that we have commit for. Not necessarily unique. -1 if none.
-	CatchupCommitRound int32 `json:"catchup_commit_round"`
-
-	// All commit precommits peer has for this height & CatchupCommitRound
-	CatchupCommit *bits.BitArray `json:"catchup_commit"`
+    Height                     int64          `json:"height"`
+    Round                      int32          `json:"round"`
+    Step                       RoundStepType  `json:"step"`
+    StartTime                  time.Time      `json:"start_time"`
+    
+    // Proposal state
+    Proposal                   bool           `json:"proposal"`
+    ProposalBlockPartSetHeader types.PartSetHeader `json:"proposal_block_part_set_header"`
+    ProposalBlockParts        *bits.BitArray  `json:"proposal_block_parts"`
+    ProposalPOLRound          int32          `json:"proposal_pol_round"`
+    ProposalPOL              *bits.BitArray  `json:"proposal_pol"`
+    
+    // Voting state
+    Prevotes                  *bits.BitArray  `json:"prevotes"`
+    Precommits                *bits.BitArray  `json:"precommits"`
+    
+    // Commit state
+    LastCommitRound           int32          `json:"last_commit_round"`
+    LastCommit                *bits.BitArray  `json:"last_commit"`
+    CatchupCommitRound        int32          `json:"catchup_commit_round"`
+    CatchupCommit             *bits.BitArray  `json:"catchup_commit"`
 }
 
-// String returns a string representation of the PeerRoundState
+const (
+    defaultIndent = ""
+    stringFormat = `PeerRoundState{
+%s  Height/Round/Step: %d/%d/%v @%v
+%s  Proposal: %v -> %v
+%s  POL: %v (round %d)
+%s  Votes: prevotes=%v precommits=%v
+%s  Last Commit: %v (round %d)
+%s  Catchup: %v (round %d)
+%s}`
+)
+
+// String returns a string representation of PeerRoundState
 func (prs PeerRoundState) String() string {
-	return prs.StringIndented("")
+    return prs.StringIndented(defaultIndent)
 }
 
-// StringIndented returns a string representation of the PeerRoundState
+// StringIndented returns an indented string representation of PeerRoundState
 func (prs PeerRoundState) StringIndented(indent string) string {
-	return fmt.Sprintf(`PeerRoundState{
-%s  %v/%v/%v @%v
-%s  Proposal %v -> %v
-%s  POL      %v (round %v)
-%s  Prevotes   %v
-%s  Precommits %v
-%s  LastCommit %v (round %v)
-%s  Catchup    %v (round %v)
-%s}`,
-		indent, prs.Height, prs.Round, prs.Step, prs.StartTime,
-		indent, prs.ProposalBlockPartSetHeader, prs.ProposalBlockParts,
-		indent, prs.ProposalPOL, prs.ProposalPOLRound,
-		indent, prs.Prevotes,
-		indent, prs.Precommits,
-		indent, prs.LastCommit, prs.LastCommitRound,
-		indent, prs.CatchupCommit, prs.CatchupCommitRound,
-		indent)
+    return fmt.Sprintf(stringFormat,
+        indent, prs.Height, prs.Round, prs.Step, prs.StartTime.Format(time.RFC3339),
+        indent, prs.ProposalBlockPartSetHeader, prs.ProposalBlockParts,
+        indent, prs.ProposalPOL, prs.ProposalPOLRound,
+        indent, prs.Prevotes, prs.Precommits,
+        indent, prs.LastCommit, prs.LastCommitRound,
+        indent, prs.CatchupCommit, prs.CatchupCommitRound,
+        indent,
+    )
+}
+
+// NewPeerRoundState creates a new PeerRoundState instance
+func NewPeerRoundState(height int64, round int32) *PeerRoundState {
+    return &PeerRoundState{
+        Height:              height,
+        Round:              round,
+        Step:               RoundStepPropose,
+        StartTime:          time.Now().UTC(),
+        ProposalPOLRound:   -1,
+        LastCommitRound:    -1,
+        CatchupCommitRound: -1,
+        Prevotes:          bits.NewBitArray(0),
+        Precommits:        bits.NewBitArray(0),
+        LastCommit:        bits.NewBitArray(0),
+        CatchupCommit:     bits.NewBitArray(0),
+    }
+}
+
+// IsValid performs basic validation of the peer round state
+func (prs PeerRoundState) IsValid() bool {
+    return prs.Height > 0 && 
+           prs.Round >= -1 && 
+           prs.ProposalPOLRound >= -1 &&
+           prs.LastCommitRound >= -1 &&
+           prs.CatchupCommitRound >= -1
+}
+
+// HasProposal checks if the peer has a complete proposal
+func (prs PeerRoundState) HasProposal() bool {
+    return prs.Proposal && 
+           prs.ProposalBlockParts != nil && 
+           prs.ProposalBlockParts.All()
+}
+
+// HasPrevoteQuorum checks if the peer has +2/3 prevotes
+func (prs PeerRoundState) HasPrevoteQuorum() bool {
+    return prs.Prevotes != nil && 
+           prs.Prevotes.HasTwoThirdsMajority()
+}
+
+// HasPrecommitQuorum checks if the peer has +2/3 precommits
+func (prs PeerRoundState) HasPrecommitQuorum() bool {
+    return prs.Precommits != nil && 
+           prs.Precommits.HasTwoThirdsMajority()
 }
