2241e18e8a3a01c5048bf6fc055a7d3025070863
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-12-01 19:52:25 +0200

    Update proposal.go

--- types/proposal.go
+++ types/proposal.go
@@ -16,12 +16,6 @@ var (
 	ErrInvalidBlockPartHash      = errors.New("error invalid block part hash")
 )
 
-// Proposal defines a block proposal for the consensus.
-// It refers to the block by BlockID field.
-// It must be signed by the correct proposer for the given Height/Round
-// to be considered valid. It may depend on votes from a previous round,
-// a so-called Proof-of-Lock (POL) round, as noted in the POLRound.
-// If POLRound >= 0, then BlockID corresponds to the block that is locked in POLRound.
 type Proposal struct {
 	Type      cmtproto.SignedMsgType
 	Height    int64     `json:"height"`
