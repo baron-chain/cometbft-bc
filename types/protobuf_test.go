3dee06bd2b14286640335ba1890e40144f66812b
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-12-01 20:01:50 +0200

    Update protobuf_test.go

--- types/protobuf_test.go
+++ types/protobuf_test.go
@@ -1,86 +1,86 @@
 package types
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-
-	abci "github.com/cometbft/cometbft/abci/types"
-	"github.com/cometbft/cometbft/crypto"
-	"github.com/cometbft/cometbft/crypto/ed25519"
-	cryptoenc "github.com/cometbft/cometbft/crypto/encoding"
+    "testing"
+    
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/require"
+    
+    abci "github.com/baron-chain/cometbft-bc/abci/types"
+    "github.com/baron-chain/cometbft-bc/crypto"
+    "github.com/baron-chain/cometbft-bc/crypto/kyber"
+    cryptoenc "github.com/baron-chain/cometbft-bc/crypto/encoding"
 )
 
 func TestABCIPubKey(t *testing.T) {
-	pkEd := ed25519.GenPrivKey().PubKey()
-	err := testABCIPubKey(t, pkEd, ABCIPubKeyTypeEd25519)
-	assert.NoError(t, err)
-}
-
-func testABCIPubKey(t *testing.T, pk crypto.PubKey, typeStr string) error {
-	abciPubKey, err := cryptoenc.PubKeyToProto(pk)
-	require.NoError(t, err)
-	pk2, err := cryptoenc.PubKeyFromProto(abciPubKey)
-	require.NoError(t, err)
-	require.Equal(t, pk, pk2)
-	return nil
+    pk := kyber.GenPrivKey().PubKey()
+    abciPubKey, err := cryptoenc.PubKeyToProto(pk)
+    require.NoError(t, err)
+    
+    pk2, err := cryptoenc.PubKeyFromProto(abciPubKey)
+    require.NoError(t, err)
+    require.Equal(t, pk, pk2)
 }
 
 func TestABCIValidators(t *testing.T) {
-	pkEd := ed25519.GenPrivKey().PubKey()
-
-	// correct validator
-	tmValExpected := NewValidator(pkEd, 10)
-
-	tmVal := NewValidator(pkEd, 10)
-
-	abciVal := TM2PB.ValidatorUpdate(tmVal)
-	tmVals, err := PB2TM.ValidatorUpdates([]abci.ValidatorUpdate{abciVal})
-	assert.Nil(t, err)
-	assert.Equal(t, tmValExpected, tmVals[0])
-
-	abciVals := TM2PB.ValidatorUpdates(NewValidatorSet(tmVals))
-	assert.Equal(t, []abci.ValidatorUpdate{abciVal}, abciVals)
-
-	// val with address
-	tmVal.Address = pkEd.Address()
-
-	abciVal = TM2PB.ValidatorUpdate(tmVal)
-	tmVals, err = PB2TM.ValidatorUpdates([]abci.ValidatorUpdate{abciVal})
-	assert.Nil(t, err)
-	assert.Equal(t, tmValExpected, tmVals[0])
+    pk := kyber.GenPrivKey().PubKey()
+    expectedVal := NewValidator(pk, 10)
+    
+    t.Run("Basic Validator Update", func(t *testing.T) {
+        val := NewValidator(pk, 10)
+        abciVal := TM2PB.ValidatorUpdate(val)
+        vals, err := PB2TM.ValidatorUpdates([]abci.ValidatorUpdate{abciVal})
+        
+        assert.NoError(t, err)
+        assert.Equal(t, expectedVal, vals[0])
+        
+        abciVals := TM2PB.ValidatorUpdates(NewValidatorSet(vals))
+        assert.Equal(t, []abci.ValidatorUpdate{abciVal}, abciVals)
+    })
+    
+    t.Run("Validator with Address", func(t *testing.T) {
+        val := NewValidator(pk, 10)
+        val.Address = pk.Address()
+        abciVal := TM2PB.ValidatorUpdate(val)
+        vals, err := PB2TM.ValidatorUpdates([]abci.ValidatorUpdate{abciVal})
+        
+        assert.NoError(t, err)
+        assert.Equal(t, expectedVal, vals[0])
+    })
 }
 
-type pubKeyEddie struct{}
+type mockPubKey struct{}
 
-func (pubKeyEddie) Address() Address                            { return []byte{} }
-func (pubKeyEddie) Bytes() []byte                               { return []byte{} }
-func (pubKeyEddie) VerifySignature(msg []byte, sig []byte) bool { return false }
-func (pubKeyEddie) Equals(crypto.PubKey) bool                   { return false }
-func (pubKeyEddie) String() string                              { return "" }
-func (pubKeyEddie) Type() string                                { return "pubKeyEddie" }
+func (mockPubKey) Address() Address                            { return []byte{} }
+func (mockPubKey) Bytes() []byte                              { return []byte{} }
+func (mockPubKey) VerifySignature([]byte, []byte) bool        { return false }
+func (mockPubKey) Equals(crypto.PubKey) bool                  { return false }
+func (mockPubKey) String() string                             { return "" }
+func (mockPubKey) Type() string                               { return "mockPubKey" }
 
-func TestABCIValidatorFromPubKeyAndPower(t *testing.T) {
-	pubkey := ed25519.GenPrivKey().PubKey()
-
-	abciVal := TM2PB.NewValidatorUpdate(pubkey, 10)
-	assert.Equal(t, int64(10), abciVal.Power)
-
-	assert.Panics(t, func() { TM2PB.NewValidatorUpdate(nil, 10) })
-	assert.Panics(t, func() { TM2PB.NewValidatorUpdate(pubKeyEddie{}, 10) })
+func TestValidatorUpdate(t *testing.T) {
+    pubkey := kyber.GenPrivKey().PubKey()
+    
+    t.Run("Valid Update", func(t *testing.T) {
+        abciVal := TM2PB.NewValidatorUpdate(pubkey, 10)
+        assert.Equal(t, int64(10), abciVal.Power)
+    })
+    
+    t.Run("Invalid Updates", func(t *testing.T) {
+        assert.Panics(t, func() { TM2PB.NewValidatorUpdate(nil, 10) })
+        assert.Panics(t, func() { TM2PB.NewValidatorUpdate(mockPubKey{}, 10) })
+    })
 }
 
-func TestABCIValidatorWithoutPubKey(t *testing.T) {
-	pkEd := ed25519.GenPrivKey().PubKey()
-
-	abciVal := TM2PB.Validator(NewValidator(pkEd, 10))
-
-	// pubkey must be nil
-	tmValExpected := abci.Validator{
-		Address: pkEd.Address(),
-		Power:   10,
-	}
-
-	assert.Equal(t, tmValExpected, abciVal)
+func TestValidatorWithoutPubKey(t *testing.T) {
+    pk := kyber.GenPrivKey().PubKey()
+    val := NewValidator(pk, 10)
+    abciVal := TM2PB.Validator(val)
+    
+    expected := abci.Validator{
+        Address: pk.Address(),
+        Power:   10,
+    }
+    
+    assert.Equal(t, expected, abciVal)
 }
