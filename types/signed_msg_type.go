de01b1ad3f0536ac956c0948ce647ed6b8306050
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-12-01 22:14:33 +0200

    Update signed_msg_type.go

--- types/signed_msg_type.go
+++ types/signed_msg_type.go
@@ -2,7 +2,6 @@ package types
 
 import cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 
-// IsVoteTypeValid returns true if t is a valid vote type.
 func IsVoteTypeValid(t cmtproto.SignedMsgType) bool {
 	switch t {
 	case cmtproto.PrevoteType, cmtproto.PrecommitType:
