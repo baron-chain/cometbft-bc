113a3bafee5a41116e1ff61a469533ce50ced5fc
BARON CHAIN <154281278+baron-chain@users.noreply.github.com>
2024-12-03 20:24:48 +0200

    Update utils.go

--- types/utils.go
+++ types/utils.go
@@ -1,29 +1,54 @@
 package types
 
-import "reflect"
+import (
+    "reflect"
+    "github.com/baron-chain/cometbft-bc/libs/log"
+)
 
+// isTypedNil efficiently checks if an interface is nil while handling typed nils
 func isTypedNil(o interface{}) bool {
-   if o == nil {
-       return false
-   }
-   rv := reflect.ValueOf(o)
-   switch rv.Kind() {
-   case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.Slice:
-       return rv.IsNil()
-   default:
-       return false
-   }
+    if o == nil {
+        return true
+    }
+    
+    rv := reflect.ValueOf(o)
+    switch rv.Kind() {
+    case reflect.Ptr, reflect.Interface, reflect.Chan, reflect.Map, reflect.Slice:
+        return rv.IsNil()
+    default:
+        return false
+    }
 }
 
+// isEmpty efficiently checks if a value is empty, optimized for blockchain data structures
 func isEmpty(o interface{}) bool {
-   if o == nil {
-       return true
-   }
-   rv := reflect.ValueOf(o)
-   switch rv.Kind() {
-   case reflect.Array, reflect.Chan, reflect.Map, reflect.Slice, reflect.String:
-       return rv.Len() == 0
-   default:
-       return false
-   }
+    if o == nil {
+        return true
+    }
+
+    rv := reflect.ValueOf(o)
+    switch rv.Kind() {
+    case reflect.String:
+        return rv.Len() == 0
+    case reflect.Array, reflect.Map, reflect.Slice:
+        return rv.Len() == 0
+    case reflect.Ptr:
+        if rv.IsNil() {
+            return true
+        }
+        return isEmpty(rv.Elem().Interface())
+    default:
+        return false
+    }
+}
+
+// ValidateEmpty validates if a blockchain data structure is empty
+func ValidateEmpty(data interface{}, logger log.Logger) bool {
+    if isEmpty(data) {
+        if logger != nil {
+            logger.Debug("validation failed: empty data structure")
+        }
+        return true
+    }
+    return false
 }
